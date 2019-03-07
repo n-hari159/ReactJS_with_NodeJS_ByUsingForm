5b444d757d07b262d5db3d50080097cc4ddad350
n-hari159 <n.hari159@gmail.com>
2019-03-07 12:12:34 +0530

    Updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const bodyParser=require('body-parser');
 const dbConnect= require("./dbConnect");
 const Visitor = require("./models/visitor");
+const path=require('path');
 
 const app=express();
 const port=process.env.PORT||1234;
@@ -9,8 +10,11 @@ const port=process.env.PORT||1234;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(express.static(path.join(__dirname,"/Views")));
+
+
 app.get("/", function(req,res,next){
-    res.sendFile(__dirname+"/reactcrash/public/")
+    res.sendFile(__dirname+"/Views/build/index.html");
 })
 app.post("/",function(req,res,next){
     var user = new Visitor();
