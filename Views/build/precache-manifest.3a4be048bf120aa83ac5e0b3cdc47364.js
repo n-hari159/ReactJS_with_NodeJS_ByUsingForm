f7a645d30a037b2989aae480ec757992ae3c7aad
n-hari159 <n.hari159@gmail.com>
2019-03-07 12:11:30 +0530

    Build folder added into views

---
+++ Views/build/precache-manifest.3a4be048bf120aa83ac5e0b3cdc47364.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e57af6ec884772d658e1",
+    "url": "/static/js/main.e57af6ec.chunk.js"
+  },
+  {
+    "revision": "fe7b5a4cd2c486c409e5",
+    "url": "/static/js/2.fe7b5a4c.chunk.js"
+  },
+  {
+    "revision": "e57af6ec884772d658e1",
+    "url": "/static/css/main.d73e51c4.chunk.css"
+  },
+  {
+    "revision": "48afdc4805c1ffda08742ce6bdd35f68",
+    "url": "/index.html"
+  }
+];
